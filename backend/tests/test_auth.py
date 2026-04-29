f25a5a24dafd5bb5990ca421ca8dd6e25c6e215f
Jose P <josepinzon0029@gmail.com>
2026-04-28 22:07:38 -0500

    terminado etapa test

--- backend/tests/test_auth.py
+++ backend/tests/test_auth.py
@@ -1,11 +1,31 @@
 from fastapi.testclient import TestClient
 from main import app
+import uuid
+
 
 client = TestClient(app)
 
 def test_login():
-    response = client.post("/login", json={
+    response = client.post("/api/auth/login", json={
         "email": "test@test.com",
         "password": "123456"
     })
     assert response.status_code in [200, 401]
+
+def test_register_and_login():
+    email = f"{uuid.uuid4()}@test.com"
+
+    register_response = client.post("/api/auth/register", json={
+        "organization_name": "Test Org",  
+        "email": email,
+        "password": "123456"
+    })
+
+    assert register_response.status_code == 201  
+
+    login_response = client.post("/api/auth/login", json={
+        "email": email,
+        "password": "123456"
+    })
+
+    assert login_response.status_code == 200
